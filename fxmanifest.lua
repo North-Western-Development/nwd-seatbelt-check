802f547ef5078e1f7c62877b284599cffb927d3b
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2024-02-21 13:51:53 -0900

    Initial Script Template

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Jackson Abney <jackson.abney@northwesterndevelopment.com>'
+description 'Script Template'
+version '1.0.0'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+shared_scripts {
+    'shared/config.lua'
+}
+
+lua54 'yes'
